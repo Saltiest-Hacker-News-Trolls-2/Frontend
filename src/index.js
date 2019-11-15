4e55e5ba4b3426d330031f7979518ccaead7a637
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 18:50:05 -0500

    import reset.css + normalize.css

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 /// styles ///
-// import 'reset-css/reset.css';
-// import 'normalize-css/normalize.css';
+import 'reset-css/reset.css';
+import 'normalize-css/normalize.css';
 import './styles/index.css';
 import './styles/basics.css';
 // import './styles/generics.css';
