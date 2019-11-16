d79fe9310bd132481c972dd018393c7dafddfedb
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 19:12:42 -0500

    import from react-router-dom ; add nav links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /// external modules ///
 import React from 'react';
+import { Route , NavLink } from 'react-router-dom';
 
 /// styles ///
 import './styles/App.css';
@@ -12,6 +13,14 @@ function App () {
     <div className='App'>
       <header>
         <h1>TODO</h1>
+        <nav>
+          <ul>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/user/account'>Account</NavLink>
+            <NavLink to='/user/sign-in'>Sign In</NavLink>
+            <NavLink to='/user/sign-up'>Sign Up</NavLink>
+          </ul>
+        </nav>
       </header>
       <main>
         TODO
