27a256f849b21873711bfa8255e9cf5f4ed3b8f6
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 19:27:35 -0500

    add about page component

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  COMPONENT
+***************************************/
+const About = (props) => {
+  return (
+    <section id='about' className='page'>
+      <header>
+        <h2>About</h2>
+      </header>
+      <main>
+        TODO
+      </main>
+      <footer></footer>
+    </section>
+  );
+};
+
+/**************************************/
+export default About;
