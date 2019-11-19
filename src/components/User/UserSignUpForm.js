5e1f4029289312aa11a2e5a3fa397ef78eb37f86
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-19 14:50:23 -0500

    return missing form field confirmation

--- src/components/User/UserSignUpForm.js
+++ src/components/User/UserSignUpForm.js
@@ -21,15 +21,24 @@ const UserSignUpForm = (props) => {
         <FormItem {...props} name='username'>
           <Field type='text' name='username' placeholder='Username'/>
         </FormItem>
+        <FormItem {...props} name='confirm_username'>
+          <Field type='text' name='confirm_username' placeholder='Confirm Your Username'/>
+        </FormItem>
         <FormItem {...props} name='email'>
           <Field type='email' name='email' placeholder='Email'/>
+        </FormItem>
+        <FormItem {...props} name='confirm_email'>
+          <Field type='email' name='confirm_email' placeholder='Confirm Your Email'/>
         </FormItem>
         <FormItem {...props} name='password'>
           <Field type='password' name='password' placeholder='Password'/>
         </FormItem>
-        {/* <FormItem {...props} name='tos'>
-          <Field type='checkbox' name='tos' checked={props.values.tos}/>
-          <label htmlFor="tos">I accept the <a href="#">Terms of Service</a>.</label>
+        <FormItem {...props} name='confirm_password'>
+          <Field type='password' name='confirm_password' placeholder='Confirm Your Password'/>
+        </FormItem>
+        {/* <FormItem {...props} name='agree_to_terms'>
+          <Field type='checkbox' name='agree_to_terms' checked={props.values.agree_to_terms}/>
+          <label htmlFor='agree_to_terms'>I accept the <a href='#'>Terms of Service</a>.</label>
         </FormItem> */}
         <FormItem {...props} name='submit'>
           <button type='submit' name='submit'>Submit</button>
