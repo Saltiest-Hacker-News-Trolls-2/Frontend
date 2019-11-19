a26861276a96677b620bbab1c2599a0218cd81cd
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-19 13:30:14 -0500

    implement Form-helpers in UserSignUpForm

--- src/components/User/UserSignUpForm.js
+++ src/components/User/UserSignUpForm.js
@@ -4,6 +4,7 @@ import { withFormik , Form , Field } from 'formik';
 import * as Yup from 'yup';
 
 /// internal modules ///
+import * as fi from './Form-helpers';
 import Card from '../generics/cards/Card';
 import CardHead from '../generics/cards/CardHead';
 import CardBody from '../generics/cards/CardBody';
@@ -68,60 +69,17 @@ const UserSignUpForm = (props) => {
 };
 
 const FormikUserSignUpForm = withFormik ({
-  mapPropsToValues : (values) => ({
-    'username'         : values.username         || init.username.default,
-    'confirm_username' : values.confirm_username || init.username.default,
-    'email'            : values.email            || init.email.default,
-    'confirm_email'    : values.confirm_email    || init.email.default,
-    'password'         : values.password         || init.password.default,
-    'confirm_password' : values.confirm_password || init.password.default,
-    /* 'agree_to_terms' : values.agree_to_terms || init.agree_to_terms.default, */
-  }),
-  validationSchema : Yup.object ().shape ({
-    'username' : Yup.string ()
-      .required ('You must provide your username.')
-      .trim (),
-    'confirm_username' : Yup.string ()
-      .required ('You must confirm your username.')
-      .trim ()
-      .oneOf ([Yup.ref ('username') , null] , 'Those usernames do not match.'),
-    'email' : Yup.string ()
-      .required ('You must provide your email.')
-      .trim ()
-      .email ('That email address is not valid.'),
-    'confirm_email' : Yup.string ()
-      .required ('You must confirm your email.')
-      .trim ()
-      .oneOf ([Yup.ref ('email') , null] , 'Those emails do not match.'),
-    'password' : Yup.string ()
-      .required ('You must provide a password.')
-      .min (init.password.min_length, `Your password must be no less than ${init.password.min_length} characters long.`)
-      .max (init.password.max_length, `Your password must be no more than ${init.password.max_length} characters long.`),
-    'confirm_password' : Yup.string ()
-      .required ('You must confirm your password.')
-      .oneOf ([Yup.ref ('password') , null] , 'Those passwords do not match.'),
-    /*
-    'agree_to_terms' : Yup.boolean ()
-      .required ('You must choose whether to accept the Terms of Service.')
-      .oneOf ([true] , 'You must accept the Terms of Service.'),
-    */
-  }),
-  handleSubmit : (values, { props : { submit } , setSubmitting , resetForm }) => {
-    try {
-      console.log ('--- submitting... ---');
-      console.log (values);
-      submit (values);
-      console.log ('--- success! ---');
-      resetForm ();
-    }
-    catch (error) {
-      console.log ('--- failure! ---')
-      console.log (error);
-    }
-    finally {
-      setSubmitting (false);
-    }
-  }
+  mapPropsToValues : fi.mapPropsToValues,
+  validationSchema : Yup.object ().shape (fi.partialSchemaShape ([
+    'username' , 'confirm_username' ,
+    'email'    , 'confirm_email'    ,
+    'password' , 'confirm_password' ,
+  ])),
+  handleSubmit : fi.handlePartialSubmit ([
+    'username' ,
+    'email'    ,
+    'password' ,
+  ]),
 }) (UserSignUpForm);
 
 /**************************************/
