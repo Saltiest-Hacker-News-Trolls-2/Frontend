b9cbcf62e6560b990a8c83ee560970f5166930c3
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-18 20:20:54 -0500

    add filterable versions of fields, schema, handleSubmit

--- src/components/User/manage-form.js
+++ src/components/User/manage-form.js
@@ -17,7 +17,7 @@ const entriesByName = (name) => ([ key ]) => (name.includes (key));
 /***************************************
   RECOGNIZED FIELDS
 ***************************************/
-export const fields = ({
+export const allFields = ({
   'username' : {
     'default' : '',
   },
@@ -34,23 +34,27 @@ export const fields = ({
   },
 });
 
+export const fields = (names) => (
+  Object.filter (allFields , entriesByName (names))
+);
+
 /***************************************
   MAP PROPS TO VALUES
 ***************************************/
 export const map = (values) => ({
-  'username'         : values.username         || fields.username.default,
-  'confirm_username' : values.confirm_username || fields.username.default,
-  'email'            : values.email            || fields.email.default,
-  'confirm_email'    : values.confirm_email    || fields.email.default,
-  'password'         : values.password         || fields.password.default,
-  'confirm_password' : values.confirm_password || fields.password.default,
-  'agree_to_terms'   : values.agree_to_terms   || fields.agree_to_terms.default,
+  'username'         : values.username         || allFields.username.default,
+  'confirm_username' : values.confirm_username || allFields.username.default,
+  'email'            : values.email            || allFields.email.default,
+  'confirm_email'    : values.confirm_email    || allFields.email.default,
+  'password'         : values.password         || allFields.password.default,
+  'confirm_password' : values.confirm_password || allFields.password.default,
+  'agree_to_terms'   : values.agree_to_terms   || allFields.agree_to_terms.default,
 });
 
 /***************************************
   VALIDATION SCHEMA
 ***************************************/
-export const schema = ({
+export const allSchema = ({
   'username' : Yup.string ()
     .required ('You must provide your username.')
     .trim (),
@@ -68,8 +72,8 @@ export const schema = ({
     .oneOf ([Yup.ref ('email') , null] , 'Those emails do not match.'),
   'password' : Yup.string ()
     .required ('You must provide a password.')
-    .min (fields.password.min_length , `Your password must be no less than ${fields.password.min_length} characters long.`)
-    .max (fields.password.max_length , `Your password must be no more than ${fields.password.max_length} characters long.`),
+    .min (allFields.password.min_length , `Your password must be no less than ${allFields.password.min_length} characters long.`)
+    .max (allFields.password.max_length , `Your password must be no more than ${allFields.password.max_length} characters long.`),
   'confirm_password' : Yup.string ()
     .required ('You must confirm your password.')
     .oneOf ([Yup.ref ('password') , null] , 'Those passwords do not match.'),
@@ -78,15 +82,20 @@ export const schema = ({
     .oneOf ([true] , 'You must accept the Terms of Service.'),
 });
 
+export const schema = (names) => (
+  Object.filter (allSchema , entriesByName (names))
+);
+
 /***************************************
   HANDLE SUBMIT
 ***************************************/
-export const handleSubmit = (values, { props : { submit } , setSubmitting , resetForm }) => {
+export const handleSubmit = (names) => (values, { props : { submit } , setSubmitting , resetForm }) => {
 
   try {
+    const valuesToSubmit = Object.filter (values , entriesByName (names));
     console.log ('--- submitting... ---');
-    console.log (values);
-    submit (values);
+    console.log (valuesToSubmit);
+    submit (valuesToSubmit);
     console.log ('--- success! ---');
     resetForm ();
   }
