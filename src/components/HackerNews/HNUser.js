3274e095c0c378de0d1e482dd9734b5597cbd41a
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 16:11:27 -0500

    add comments detailing component props

--- src/components/HackerNews/HNUser.js
+++ src/components/HackerNews/HNUser.js
@@ -5,12 +5,25 @@ import React from 'react';
   COMPONENT
 ***************************************/
 const HNUser = ({
+  /* data :: object
+  -- data as-is (from HackerNews API) */
   data,
-  /* optional: */ isFav = null,
-  /* optional: */ toggleFav = null,
+  /* [optional] isFav :: boolean
+  -- current fav status (from Web/backend API) */
+  isFav = null,
+  /* [optional] toggleFav :: function
+  -- callback to add/remove fav status (in Web/frontend + Web/backend API) */
+  toggleFav = null,
+  /* view :: function
+  -- callback to view item (in Web/frontend) */
   view,
+  /* score :: number
+  -- saltiness score of item (from DS API) */
   score,
-  /* optional: */ tags = null,
+  /* [optional] tags :: array
+  -- list of tags classifying item (from DS API) */
+  tags = null,
+  /* rest of props */
   ...props
 }) => {
   return (
