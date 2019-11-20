00df9ea0e9385a28749cc558941139d82b167963
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 15:41:01 -0500

    add blank HackerNews-related components

---
+++ src/components/HackerNews/HNUser.js
@@ -0,0 +1,14 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  COMPONENT
+***************************************/
+const HNUser = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+/**************************************/
+export default HNUser;
