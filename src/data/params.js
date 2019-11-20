1fff21d67624601459cf3a40cb0ecebc7df91260
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 14:26:50 -0500

    add objs/funs for getting param values from urls

---
+++ src/data/params.js
@@ -0,0 +1,85 @@
+/*******************************************************************************
+  PARAMS
+*******************************************************************************/
+
+/*//////////////////////////////////////
+  utils
+//////////////////////////////////////*/
+
+Array.prototype.first = function () {
+  return (this[0]);
+};
+
+Array.prototype.last = function () {
+  return (this[this.length - 1]);
+};
+
+Array.prototype.rest = function () {
+  return (this.slice (1 , this.length));
+}
+
+/***************************************
+  main
+***************************************/
+export const pattern = (type , name) => {
+  const make = (s) => (new RegExp (s));
+  switch (type) {
+    case '/' :
+      return (make (`.*[/]${name}/([^/]+).*`));
+    case '?' :
+      return (make (`.*[?&]${name}=([^&]+).*`));
+    default :
+      return (null);
+  }
+}
+
+/*h3
+export const params = {
+  match : function (type , name , url) {
+    /// get pattern ///
+    const pat = pattern (type , name);
+    
+    /// get matches ///
+    if (pat) { return (pat.exec (url).rest ()); }
+    
+    /// default ///
+    return (null);
+  },
+  matchFirst : function (type , name , url) {
+    return (this.match (type , name , url).first ())
+  },
+  matchLast : function (type , name , url) {
+    return (this.match (type , name , url).last ())
+  },
+};
+
+/*******************
+  URL PARAMS
+*******************/
+export const urlParams = {
+  match      : (name , url) => (params.match ('/' , name , url)),
+  matchFirst : (name , url) => (params.matchFirst ('/' , name , url)),
+  matchLast  : (name , url) => (params.matchLast ('/' , name , url)),
+};
+
+/*******************
+  QUERY PARAMS
+*******************/
+export const queryParams = {
+  match      : (name , url) => (params.match ('?' , name , url)),
+  matchFirst : (name , url) => (params.matchFirst ('?' , name , url)),
+  matchLast  : (name , url) => (params.matchLast ('?' , name , url)),
+};
+
+/***************************************
+  GET VALUES FROM STANDARD INFO
+***************************************/
+export const getQueryPage = (info) => {
+  let page = null;
+  if (page === null && info.prev !== '') {
+    page = queryParams.matchLast ('page' , info.prev);
+  }
+  if (page === null && info.next !== '') {
+    page = queryParams.matchLast ('page' , info.next);
+  }
+};
