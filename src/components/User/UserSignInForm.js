46731a7e628235b1f38eed49e73909e189c71582
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 17:16:42 -0500

    use CardHead, CardBody, CardFoot in forms

--- src/components/User/UserSignInForm.js
+++ src/components/User/UserSignInForm.js
@@ -6,9 +6,9 @@ import * as Yup from 'yup';
 /// internal modules ///
 import * as fi from './Form-helpers';
 import Card from '../generics/cards/Card';
-// import CardHead from '../generics/cards/CardHead';
-// import CardBody from '../generics/cards/CardBody';
-// import CardFoot from '../generics/cards/CardFoot';
+import CardHead from '../generics/cards/CardHead';
+import CardBody from '../generics/cards/CardBody';
+import CardFoot from '../generics/cards/CardFoot';
 import FormItem from '../generics/forms/FormItem';
 
 /***************************************
@@ -17,20 +17,26 @@ import FormItem from '../generics/forms/FormItem';
 const UserSignInForm = (props) => {
   return (
     <Card>
-      <Form className='user-sign-in'>
-        <FormItem {...props} name='username'>
-          <Field type='text' name='username' placeholder='Username'/>
-        </FormItem>
-        {/* <FormItem {...props} name='email'>
-          <Field type='email' name='email' placeholder='Email'/>
-        </FormItem> */}
-        <FormItem {...props} name='password'>
-          <Field type='password' name='password' placeholder='Password'/>
-        </FormItem>
-        <FormItem {...props} name='submit'>
-          <button type='submit' name='submit'>Submit</button>
-        </FormItem>
-      </Form>
+      <CardHead>
+        <h2>Sign In</h2>
+      </CardHead>
+      <CardBody>
+        <Form className='user-sign-in'>
+          <FormItem {...props} name='username'>
+            <Field type='text' name='username' placeholder='Username'/>
+          </FormItem>
+          {/* <FormItem {...props} name='email'>
+            <Field type='email' name='email' placeholder='Email'/>
+          </FormItem> */}
+          <FormItem {...props} name='password'>
+            <Field type='password' name='password' placeholder='Password'/>
+          </FormItem>
+          <FormItem {...props} name='submit'>
+            <button type='submit' name='submit'>Submit</button>
+          </FormItem>
+        </Form>
+      </CardBody>
+      <CardFoot></CardFoot>
     </Card>
   );
 };
