4eca43627da02e946c680bc31b1c02fd211495e0
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-21 02:13:26 -0500

    build out HNItem component

--- src/components/HackerNews/HNItem.js
+++ src/components/HackerNews/HNItem.js
@@ -36,7 +36,27 @@ const HNItem = ({
   ...rest
 }) => {
   return (
-    <div></div>
+    <Card className='HN-item t-to-b'
+      col
+      onClick={view}
+    >
+      <CardHead>
+        {/* <span className='item-salty-score'>{score}</span> */}
+        <span className='item-type'>({itemData.type})</span>
+        <h3><span className='item-title'>{itemData.title}</span></h3>
+        <span className='item-karma-score'>({itemData.score})</span>
+        <button className='item-fav' onClick={toggleFav}>Fav? {isFav}</button>
+        <span className='item-by-user'>by {itemData.by}</span>
+        <span className='item-at-time'>at {itemData.time}</span>
+      </CardHead>
+      <CardBody>
+        <div className='item-text'>{itemData.text}</div>
+      </CardBody>
+      <CardFoot>
+        <a className='item-HN-url' href={`https://news.ycombinator.com/item?id=${itemData.id}`}>View at HackerNews</a>
+        {itemData.url && (<a className='item-origin-url' href={itemData.url}>View Original</a>)}
+      </CardFoot>
+    </Card>
   );
 };
 
