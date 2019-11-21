8ebcdf9b2ef3fb2b299dea8c653c71bc2d9681fd
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-21 03:27:00 -0500

    add example data for DS-backend

---
+++ src/data/DS-backend/example.js
@@ -0,0 +1,30 @@
+/*******************************************************************************
+  EXAMPLE DATA
+*******************************************************************************/
+
+export const base_url = "https://hackernewsapilambda.herokuapp.com/saltyuser/?format=json";
+
+export const userScores = [
+  {
+    "id" : 1,
+    "score" : 123,
+  },{
+    "id" : 2,
+    "score" : 234,
+  },{
+    "id" : 3,
+    "score" : 345,
+  },{
+    "id" : 4,
+    "score" : 456,
+  },{
+    "id" : 5,
+    "score" : 567,
+  },{
+    "id" : 6,
+    "score" : 678,
+  },{
+    "id" : 7,
+    "score" : 789,
+  }
+];
