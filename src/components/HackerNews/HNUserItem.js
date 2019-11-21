db1bcd7078715df37a0502b7915998903ebf4d05
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-21 02:19:21 -0500

    add components to deal with links to HN

---
+++ src/components/HackerNews/HNUserItem.js
@@ -0,0 +1,19 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  COMPONENT
+***************************************/
+const HNUserItem = ({ itemID , children , ...rest }) => {
+  return (
+    <a
+    className={`item-HN-url ${rest.className || ''}`.trim ()}
+    href={`https://news.ycombinator.com/item?id=${itemID}`}
+    >
+      {children}
+    </a>
+  );
+};
+
+/**************************************/
+export default HNUserItem;
---
+++ src/components/HackerNews/HNUserLink.js
@@ -0,0 +1,19 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  COMPONENT
+***************************************/
+const HNUserLink = ({ userID , children , ...rest }) => {
+  return (
+    <a
+    className={`user-HN-url ${rest.className || ''}`.trim ()}
+    href={`https://news.ycombinator.com/user?id=${userID}`}
+    >
+      {children}
+    </a>
+  );
+};
+
+/**************************************/
+export default HNUserLink;
