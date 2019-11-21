74171c3b54236eaeec87b62f16330495908f0014
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-21 03:36:03 -0500

    add "get" layer to structure

--- src/data/DS-backend/example.js
+++ src/data/DS-backend/example.js
@@ -5,27 +5,29 @@
 export const base_url = "https://hackernewsapilambda.herokuapp.com/saltyuser/?format=json";
 
 export const user_scores = {
-  "rest_url" : "",
-  "success" : [{
-    "id" : 1,
-    "score" : 123,
-  },{
-    "id" : 2,
-    "score" : 234,
-  },{
-    "id" : 3,
-    "score" : 345,
-  },{
-    "id" : 4,
-    "score" : 456,
-  },{
-    "id" : 5,
-    "score" : 567,
-  },{
-    "id" : 6,
-    "score" : 678,
-  },{
-    "id" : 7,
-    "score" : 789,
-  }],
+  "get" : {
+    "rest_url" : "",
+    "success" : [{
+      "id" : 1,
+      "score" : 123,
+    },{
+      "id" : 2,
+      "score" : 234,
+    },{
+      "id" : 3,
+      "score" : 345,
+    },{
+      "id" : 4,
+      "score" : 456,
+    },{
+      "id" : 5,
+      "score" : 567,
+    },{
+      "id" : 6,
+      "score" : 678,
+    },{
+      "id" : 7,
+      "score" : 789,
+    }],
+  },
 };
