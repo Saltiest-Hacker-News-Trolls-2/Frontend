8dbddc47fe9f7b72b00313e919a4f9d5a63ab035
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 22:14:26 -0500

    add empty Feed component, styles

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,17 @@
+/// external modules ///
+import React from 'react';
+
+/// styles ///
+import './styles.css';
+
+/***************************************
+  COMPONENT
+***************************************/
+const Feed = () => {
+  return (
+    <div></div>
+  );
+};
+
+/**************************************/
+export default Feed;
