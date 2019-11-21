d13ffee5e3e4c9450f334f20921fe8b1cc329f9f
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-20 22:23:29 -0500

    add Card as default feed item ; fix module paths

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,7 +2,8 @@
 import React from 'react';
 
 /// internal modules ///
-import CardDeck from './path/CardDeck';
+import Card from '../generics/cards/Card';
+import CardDeck from '../generics/cards/CardDeck';
 
 /// styles ///
 import './styles.css';
@@ -10,7 +11,13 @@ import './styles.css';
 /***************************************
   COMPONENT
 ***************************************/
-const Feed = ({ children , ...rest }) => {
+const Feed = ({
+  /* component :: React.Component
+  -- the component to use for feed items */
+  component = Card,
+  /* rest of props */
+  ...rest
+}) => {
   return (
     <CardDeck
       className={`feed ${rest.className}`}
