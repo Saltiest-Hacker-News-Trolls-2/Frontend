342ad38174398324963f166ab410f931bb237fb5
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-22 13:06:42 -0500

    alias: isSignedOut = isLoggedOut = not isLoggedIn

--- src/data/app-states.js
+++ src/data/app-states.js
@@ -1,2 +1,5 @@
 export const isLoggedIn = () => (localStorage.getItem ('isLoggedIn') === true);
 export const isSignedIn = isLoggedIn;
+
+export const isLoggedOut = () => (!isLoggedIn ());
+export const isSignedOut = isLoggedOut;
