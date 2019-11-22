16a62fb428ef90ce62bd59d90eadef5f73e2a1be
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-22 13:30:42 -0500

    remove single quotes from field names

--- src/data/app-routes.js
+++ src/data/app-routes.js
@@ -5,35 +5,35 @@ export const routes = {
     be : {
       base : 'https://only-salty-hackers.herokuapp.com',
       ends : {
-        'user_sign_up' : '/api/register',
-        'user_sign_in' : '/api/login',
-        'users'        : (id) => (`/api/users/${id}`),
-        'favorites'    : (id) => (`/api/users/${id}/favorites`),
+        user_sign_up : '/api/register',
+        user_sign_in : '/api/login',
+        users        : (id) => (`/api/users/${id}`),
+        favorites    : (id) => (`/api/users/${id}/favorites`),
       }
     },
     fe : {
       base : 'https://frontend.saltiest-hacker-news-trolls-2.now.sh',
       ends : {
-        'home'  : '/',
-        'about' : '/about',
-        'user_sign_up' : '/user/sign-up',
-        'user_sign_in' : '/user/sign-in',
-        'user_account' : '/user/account',
+        home  : '/',
+        about : '/about',
+        user_sign_up : '/user/sign-up',
+        user_sign_in : '/user/sign-in',
+        user_account : '/user/account',
       }
     },
     ui : {
       base : 'https://saltinator.netlify.com',
       ends : {
-        'home'  : '/',
-        'about' : '/about',
+        home  : '/',
+        about : '/about',
       }
     }
   },
   HN   : {
     base : 'https://hacker-news.firebaseio.com',
     ends : {
-      'item' : (id) => (`/v0/item/${id}`),
-      'user' : (id) => (`/v0/user/${id}`),
+      item : (id) => (`/v0/item/${id}`),
+      user : (id) => (`/v0/user/${id}`),
     }
   },
 };
